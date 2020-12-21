fc7b4d9785e253bb6570703f8b5c290af8037c01
zy7y <7631909+zy7y@user.noreply.gitee.com>
2020-12-21 22:04:01 +0800

    更新schemas -> user模型并使其兼容User - > orm

--- schemas/user.py
+++ schemas/user.py
@@ -2,13 +2,24 @@ from pydantic import BaseModel
 
 
 class UserBase(BaseModel):
-    """用户基础模型"""
+    """用户输入基础模型"""
     name: str
 
 
 class UserCreate(UserBase):
-    """用户创建模型"""
+    """用户输入创建模型"""
     passwd: str
 
 
+class UserUpdate(UserBase):
+    """用户输入更新模型"""
+    pass
 
+
+class UserDB(UserBase):
+    """数据库的基础模型"""
+    id: int = None
+
+    class Config:
+        """兼容orm模型"""
+        orm_mode = True
